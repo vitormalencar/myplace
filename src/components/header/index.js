11fa0b9ec9e9724b8eff7c99fcfa719bd081aa64
vitormalencar <vitor.maia.alencar@gmail.com>
2017-02-03 08:31:05 -0300

    update header component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,18 +1,36 @@
-import { h, Component } from 'preact';
-import { Link } from 'preact-router';
+import {h, Component} from 'preact';
+import {Link} from 'preact-router';
 import style from './style';
+import FaShoppingBag from 'react-icons/lib/fa/shopping-bag';
+import MdArrowBack from 'react-icons/lib/md/arrow-back';
 
 export default class Header extends Component {
-	render() {
+	render(props) {
 		return (
-			<header class={style.header}>
-				<h1>Preact App</h1>
-				<nav>
-					<Link href="/">Home</Link>
-					<Link href="/profile">Me</Link>
-					<Link href="/profile/john">John</Link>
-				</nav>
-			</header>
+			<div class="navigation-bar navigation-bar--material">
+				<div class="navigation-bar__left navigation-bar--material__left">
+					<span class="toolbar-button toolbar-button--material">
+						{props.showBackButton && <Link href="/">
+							<div class={style.Back_Icon}><MdArrowBack/></div>
+						</Link>}
+					</span>
+				</div>
+				<div class="navigation-bar__center navigation-bar--material__center">
+					<Link href='/'>MY-PLACE</Link>
+				</div>
+				<div class="navigation-bar__right navigation-bar--material__right">
+
+					<span class="toolbar-button toolbar-button--material">
+						{props.productCount > 0 && <span class={style.Notification_Label}>{props.productCount}</span>}
+						<Link href="/cart">
+							<div class={style.Bag_Icon}><FaShoppingBag/></div>
+						</Link>
+					</span>
+					<span class="toolbar-button toolbar-button--material">
+						<i class="zmdi zmdi-more-vert"></i>
+					</span>
+				</div>
+			</div>
 		);
 	}
 }
